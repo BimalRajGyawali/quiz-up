785c9581f6eac44d5fa175760e8c852800cebcc1
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-01-29 20:36:53 +0545

    setup: sets up a react client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QuizCreationForm from './components/QuizCreationForm';
+
+function App() {
+  return (
+   <>
+   <QuizCreationForm />
+   </>
+  );
+}
+
+export default App;
